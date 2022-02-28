13b3079ae0e558a75434f0c8a5cc40da60e6288b
sadik126 <sadikhimel04@gmail.com>
2022-02-28 22:34:12 +0600

    ms

--- player.js
+++ player.js
@@ -1,4 +1,31 @@
 const getplayer = () => {
     let inputfield = document.getElementById('getplayer')
-    getplayer = inputfield.value;
+    searchplayer = inputfield.value;
+    console.log(searchplayer);
+    inputfield.value = '';
+    let url = `https://www.thesportsdb.com/api/v1/json/2/searchplayers.php?p=${searchplayer}`;
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayplayer(data.player));
+}
+
+const displayplayer = (players) => {
+  console.log(players)
+
+  let searchplayer = document.getElementById('searchplayers');
+  players.forEach(allplayers => {
+      console.log(allplayers.strPlayer);
+      const div = document.createElement('div');
+      div.classList.add('col');
+      div.innerHTML = `<div class="card" style="width: 18rem;">
+      <img src="${allplayers.strThumb}" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">${allplayers.strPlayer}</h5>
+        <p class=" card-text">${allplayers.strDescriptionEN.slice(0,100)}...</p>
+       
+      </div>
+    </div>`;
+
+    searchplayer.appendChild(div);
+  });
 }
